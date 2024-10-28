5edc4582c4b49f3cb2db20eded0ffbe1230414ea
Hein Htoo <heinhtoozaw99@gmail.com>
2024-10-28 20:26:22 +0630

    update to pool

--- lib/mysqldb.ts
+++ lib/mysqldb.ts
@@ -7,17 +7,26 @@
  *   MySQL connection
  *
  */
-import mysql from "mysql2/promise";
+import mysql, { createPool } from "mysql2/promise";
+
+const connPool = createPool({
+  host: process.env.DATABASE_HOST,
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  waitForConnections: true,
+  connectionLimit: 3,
+  maxIdle: 3, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0,
+});
 
 let connection: mysql.Connection;
 export const createConnection = async () => {
   if (!connection) {
-    connection = await mysql.createConnection({
-      host: process.env.DATABASE_HOST,
-      user: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-    });
+    connection = await connPool.getConnection();
   }
   return connection;
 };
