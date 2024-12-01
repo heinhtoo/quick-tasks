3125b1a362d76e82bdf14854991cf4e8918fcef4
Hein Htoo <heinhtoozaw99@gmail.com>
2024-12-01 18:46:09 +0630

    add docker conf

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  env: {
+    DATABASE_HOST: process.env.DATABASE_HOST,
+    DATABASE_USER: process.env.DATABASE_USER,
+    DATABASE_PASSWORD: process.env.DATABASE_PASSWORD,
+    DATABASE_NAME: process.env.DATABASE_NAME,
+  },
+};
 
 export default nextConfig;
